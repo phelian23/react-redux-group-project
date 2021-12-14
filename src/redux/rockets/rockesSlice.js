90eb4d56672a7efab35d92231fe54b1c908efc5f
mbdesigns1989 <mbdesigns1989@yahoo.com>
2021-12-14 22:53:57 +0200

    Update redux/rockets/rockesSlice.js

--- src/redux/rockets/rockesSlice.js
+++ src/redux/rockets/rockesSlice.js
@@ -1,25 +1,44 @@
-/* eslint-disable no-param-reassign */
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const fetchRockets = createAsyncThunk('rockets/fetchAllRockets', async () => {
   const response = await axios.get('https://api.spacexdata.com/v3/rockets');
-  return response.data;
+  const usefullData = response.data.map((rocketdata) => ({
+    id: rocketdata.rocket_id,
+    name: rocketdata.rocket_name,
+    images: rocketdata.flickr_images,
+    description: rocketdata.description,
+    reserved: 'false',
+  }));
+  return usefullData;
 });
 
 export const rocketsSlice = createSlice({
   name: 'rockets',
   initialState: { entities: [], loading: 'idle' },
-  reducers: {},
+  reducers: {
+    reserve: (state, action) => {
+      const rocketToReserve = state.entities.find((rockets) => rockets.id === action.payload);
+      if (rocketToReserve.reserved === 'true') {
+        rocketToReserve.reserved = 'false';
+      } else {
+        rocketToReserve.reserved = 'true';
+      }
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchRockets.pending, (state) => {
-        state.loading = 'pending';
+        const newState = state;
+        newState.loading = 'pending';
       })
       .addCase(fetchRockets.fulfilled, (state, action) => {
-        state.entities = [...action.payload];
-        state.loading = 'idle';
+        const newState = state;
+        newState.entities = [...action.payload];
+        newState.loading = 'idle';
       });
   },
 });
+
+export const { reserve } = rocketsSlice.actions;
 export default rocketsSlice.reducer;
