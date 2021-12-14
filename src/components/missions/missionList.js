23b08ed6e114b292c42e3e7d1c4a207154630b2c
Oluwafemi <femikola23@gmail.com>
2021-12-14 13:17:19 +0400

    Add missions basic structure

---
+++ src/components/missions/missionList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MissionList = () => (
+  <tr>
+    <th>Mission</th>
+    <td>description</td>
+    <td><p>Not a member</p></td>
+    <td><button type="button">Join Mission</button></td>
+  </tr>
+);
+
+export default MissionList;
