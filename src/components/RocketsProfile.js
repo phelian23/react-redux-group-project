3fc83ebaf5f143aefd93e0e6d40d838940cd1a5a
mbdesigns1989 <mbdesigns1989@yahoo.com>
2021-12-14 23:13:21 +0200

    Add RocketsProfile

---
+++ src/components/RocketsProfile.js
@@ -0,0 +1,26 @@
+import { useSelector } from 'react-redux';
+
+const RocketProfile = () => {
+  const rockets = useSelector((state) => state.rockets.entities);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved === 'true');
+  const renderReservedRockets = () => reservedRockets.map((rocket) => (
+    <tr key={rocket.id}>
+      <th>
+        🚀 &nbsp;
+        {rocket.name}
+      </th>
+    </tr>
+  ));
+  return (
+    <div>
+      <h1>My Rockets</h1>
+      <table className="table table-hover border border-solid">
+        <tbody>
+          {renderReservedRockets()}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default RocketProfile;
