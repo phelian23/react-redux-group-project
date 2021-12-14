98675f7acd41f92cf6cb9fa00458923eb49b00b1
Oluwafemi <femikola23@gmail.com>
2021-12-14 15:57:45 +0400

    Add missions to list

--- src/components/missions/missionList.js
+++ src/components/missions/missionList.js
@@ -1,12 +1,28 @@
+/* eslint-disable camelcase */
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const MissionList = () => (
-  <tr>
-    <th>Mission</th>
-    <td>description</td>
-    <td><p>Not a member</p></td>
-    <td><button type="button">Join Mission</button></td>
-  </tr>
-);
+const MissionList = ({ mission }) => {
+  const { mission_id, mission_name, description } = mission;
+
+  return (
+    <tr>
+      <th>{mission_name}</th>
+      <td>{description}</td>
+      <td><p>Not a member</p></td>
+      <td><button type="button" itemID={mission_id}>Join Mission</button></td>
+    </tr>
+  );
+};
+
+MissionList.propTypes = {
+  mission: PropTypes.shape(
+    {
+      mission_id: PropTypes.string.isRequired,
+      mission_name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    },
+  ).isRequired,
+};
 
 export default MissionList;
--- src/components/missions/missionsPage.js
+++ src/components/missions/missionsPage.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import MissionList from './missionList';
+import { fetchMissions } from '../../redux/missions/missions';
 
-const MissionsPage = () => (
-  <table className="mission-table">
-    <tr>
-      <th className="mission-data1">Mission</th>
-      <th className="mission-data2">Description</th>
-      <th className="mission-data3">status</th>
-      <th className="mission-data4">.</th>
-    </tr>
-    <MissionList />
-  </table>
-);
+const MissionsPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, []);
+
+  const missions = useSelector((state) => state.missionReducer);
+
+  return (
+    <table className="mission-table">
+      <tr>
+        <th className="mission-data1">Mission</th>
+        <th className="mission-data2">Description</th>
+        <th className="mission-data3">status</th>
+        <th className="mission-data4">.</th>
+      </tr>
+      {missions.map((mission) => (
+        <MissionList
+          key={mission.mission_id}
+          mission={mission}
+        />
+      ))}
+    </table>
+  );
+};
 
 export default MissionsPage;
